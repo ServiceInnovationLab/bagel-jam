fcf6c19f675b0cf17d0193b33ffee46cfc4fce1a
Merrin <merrin.macleod@gmail.com>
2019-12-06 12:19:59 +1300

    add a visualisation export

---
+++ export_lda_viz.py
@@ -0,0 +1,8 @@
+from bagel.viz.viz import export_lda_viz
+
+FILE_PATTERN = 'zero-carbon-bill/input/*.txt'
+# Tweak this parameter until it brings joy
+NUMBER_OF_TOPICS = 5
+OUTPUT_PATH = 'zero-carbon-bill/index_lda.html'
+
+export_lda_viz(NUMBER_OF_TOPICS, FILE_PATTERN, OUTPUT_PATH)
